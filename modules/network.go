c63d4889019159f5808bab7b9b3c98219fe68001
Hunyadvári Péter <peter.hunyadvari@vcc.live>
2021-06-09 15:18:34 +0200

    feature: ability to use multiple network interface

--- modules/network.go
+++ modules/network.go
@@ -16,7 +16,7 @@ import (
 func init() {
 	gobar.AddModule("Network", func() gobar.ModuleInterface {
 		return &Network{
-			InterfaceName: "tun1",
+			InterfaceName: []string{"tun1"},
 			barConfig:     defaultBarConfig(),
 		}
 	})
@@ -24,7 +24,7 @@ func init() {
 
 type Network struct {
 	gobar.ModuleInterface
-	InterfaceName string `json:"InterfaceName"`
+	InterfaceName []string `json:"InterfaceName"`
 	barConfig     barConfig
 	currRx        uint64
 	currTx        uint64
@@ -41,25 +41,25 @@ func (m *Network) InitModule(config json.RawMessage, log xlog.Logger) error {
 			return err
 		}
 	}
-	m.currRx, m.currTx = m.collectData()
+	_, m.currRx, m.currTx = m.collectData()
 
 	return nil
 }
 
 func (m *Network) UpdateInfo(info gobar.BlockInfo) gobar.BlockInfo {
-	currRx, currTx := m.collectData()
-	info.ShortText = fmt.Sprintf("%s %s / %s", m.InterfaceName, byteSize(currRx-m.currRx), byteSize(currTx-m.currTx))
-	info.FullText = fmt.Sprintf("%s %s / %s", m.InterfaceName, byteSize(currRx-m.currRx), byteSize(currTx-m.currTx))
+	name, currRx, currTx := m.collectData()
+	info.ShortText = fmt.Sprintf("%s %s / %s", name, byteSize(currRx-m.currRx), byteSize(currTx-m.currTx))
+	info.FullText = fmt.Sprintf("%s %s / %s", name, byteSize(currRx-m.currRx), byteSize(currTx-m.currTx))
 	m.currRx, m.currTx = currRx, currTx
 	return info
 }
 
-func (m *Network) collectData() (uint64, uint64) {
+func (m *Network) collectData() (string, uint64, uint64) {
 	// Reference: man 5 proc, Documentation/filesystems/proc.txt in Linux source code
 	file, err := os.Open("/proc/net/dev")
 	if err != nil {
 		m.log.Warn("File open error", err)
-		return 0, 0
+		return "none", 0, 0
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -74,26 +74,30 @@ func (m *Network) collectData() (uint64, uint64) {
 			continue
 		}
 		name := strings.TrimSpace(kv[0])
-		if name != m.InterfaceName {
+		found := false
+		for _, n := range m.InterfaceName {
+			if name == n {
+				found = true
+			}
+		}
+		if !found {
 			continue
 		}
 		rxBytes, err := strconv.ParseUint(fields[0], 10, 64)
 		if err != nil {
 			m.log.Warnf("Unable to parse RX field: %s", fields[0])
-			return 0, 0
 		}
 		txBytes, err := strconv.ParseUint(fields[8], 10, 64)
 		if err != nil {
-			m.log.Warnf("Unable to parse TX field: %s", fields[0])
-			return 0, 0
+			m.log.Warnf("Unable to parse TX field: %s", fields[8])
 		}
-		return rxBytes, txBytes
+		return name, rxBytes, txBytes
 	}
 	if err := scanner.Err(); err != nil {
 		m.log.Warn("File scan error", err)
-		return 0, 0
+		return "none", 0, 0
 	}
-	return 0, 0
+	return "none", 0, 0
 }
 
 func (m Network) HandleClick(cm gobar.ClickMessage, info gobar.BlockInfo) (*gobar.BlockInfo, error) {

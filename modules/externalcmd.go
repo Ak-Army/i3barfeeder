fce94eaab7ea620ba479c70ab890588e59ade9d7
Hunyadvári Péter <peter.hunyadvari@vcc.live>
2021-06-09 15:49:48 +0200

    feature: remove script module and extend extrenalCmd module

--- modules/externalcmd.go
+++ modules/externalcmd.go
@@ -2,10 +2,9 @@ package modules
 
 import (
 	"encoding/json"
+	"github.com/Ak-Army/xlog"
 	"os/exec"
 	"strings"
-
-	"github.com/Ak-Army/xlog"
 
 	"github.com/Ak-Army/i3barfeeder/gobar"
 )
@@ -18,38 +17,85 @@ func init() {
 
 type ExternalCmd struct {
 	gobar.ModuleInterface
-	Command string `json:"command"`
-	OnClick string `json:"on_lick"`
-	onClick *exec.Cmd
+	//Command to be executed (using "/bin/sh -c [command]")
+	Exec string `json:"exec"`
+
+	// Conditional command that, if defined, needs to exit successfully
+	// before the main exec command is invoked.
+	// Default: ""
+	ExecIf string `json:"exec_if"`
+
+	//"click-(left|middle|right)" will be executed using "/bin/sh -c [command]"
+	ClickLeft   string `json:"click_left"`
+	ClickMiddle string `json:"click_middle"`
+	ClickRight  string `json:"click_right"`
+
+	// "scroll-(up|down)" will be executed using "/bin/sh -c [command]"
+	ScrollUp   string `json:"scroll_up"`
+	ScrollDown string `json:"scroll_down"`
 }
 
 func (m *ExternalCmd) InitModule(config json.RawMessage, log xlog.Logger) error {
-	if err := json.Unmarshal(config, m); err != nil {
-		return err
-	}
-	if m.OnClick != "" {
-		split := strings.Split(m.OnClick, " ")
-		m.onClick = exec.Command(split[0], split[1:]...)
+	if config != nil {
+		if err := json.Unmarshal(config, m); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-func (m ExternalCmd) UpdateInfo(info gobar.BlockInfo) gobar.BlockInfo {
-	if m.Command != "" {
-		out, err := exec.Command("sh", "-c", m.Command).Output()
+func (m *ExternalCmd) UpdateInfo(info gobar.BlockInfo) gobar.BlockInfo {
+	if m.ExecIf != "" {
+		_, err := exec.Command("sh", "-c", m.ExecIf).Output()
 		if err != nil {
-			info.FullText = err.Error()
-			info.TextColor = "#FF2222"
-		} else {
-			info.FullText = strings.TrimSpace(string(out))
+			return info
 		}
 	}
+	m.execCommand(m.Exec, &info)
+
 	return info
 }
 
-func (m ExternalCmd) HandleClick(cm gobar.ClickMessage, info gobar.BlockInfo) (*gobar.BlockInfo, error) {
-	if cm.Button == 3 && m.onClick != nil {
-		return nil, m.onClick.Start()
+func (m *ExternalCmd) HandleClick(cm gobar.ClickMessage, info gobar.BlockInfo) (*gobar.BlockInfo, error) {
+	switch cm.Button {
+	case 1: // left button
+		if m.ClickLeft != "" {
+			m.execCommand(m.ClickLeft, &info)
+			return &info, nil
+		}
+	case 2: // middle button
+		if m.ClickMiddle != "" {
+			m.execCommand(m.ClickMiddle, &info)
+			return &info, nil
+		}
+	case 3: // right click, join zoom
+		if m.ClickRight != "" {
+			m.execCommand(m.ClickRight, &info)
+			return &info, nil
+		}
+	case 4: // scroll up, decrease
+		if m.ScrollUp != "" {
+			m.execCommand(m.ScrollUp, &info)
+			return &info, nil
+		}
+	case 5: // scroll down, decrease
+		if m.ScrollDown != "" {
+			m.execCommand(m.ScrollDown, &info)
+			return &info, nil
+		}
 	}
 	return nil, nil
+}
+
+func (m *ExternalCmd) execCommand(cmd string, info *gobar.BlockInfo) {
+	out, err := exec.Command("sh", "-c", cmd).Output()
+	if err != nil {
+		info.ShortText = err.Error()
+		info.FullText = err.Error()
+		return
+	}
+	text := strings.TrimRight(string(out), "\n")
+	info.ShortText = text
+	info.FullText = text
+	return
 }
